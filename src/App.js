4525d61bd39164b220e3de5da1663f28d1e90b0a
prejmerepace <prejmer@partidul-epace.ro>
2025-10-23 13:39:44 -0700

    Add files via upload
    DEDEE

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="center-button">fdfdfafda</button>
+    </div>
+  );
+}
+
+export default App;
